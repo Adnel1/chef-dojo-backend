69eec87bbc690969de5e227763f2e54e2bc3da76
Adnel <repartomoro@hotmail.com>
2024-06-19 18:14:22 -0400

    Fix Heroku DATABASE_URL schema issue

--- database.py
+++ database.py
@@ -9,6 +9,10 @@ load_dotenv()
 # Fetch the DATABASE_URL from the environment variable
 URL_DATABASE = os.getenv('DATABASE_URL')
 
+# Correct the URL if it uses postgres://
+if URL_DATABASE and URL_DATABASE.startswith("postgres://"):
+    URL_DATABASE = URL_DATABASE.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
